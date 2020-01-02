e999c0de6943f9068b0430e3dc1e0787f2b6ffea
Justin A <jarata@gmail.com>
2020-01-02 12:36:42 -0800

    building layout on Form.js: imported formik setup fields

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {withFormik, Field, Form} from "formik";
 
-const Form = () => {
+const UserForm = () => {
 	return (
 		<div>
 			Form
+			<Form>
+				<Field type="text" name="name" placeholder="Enter your name"/>
+				<Field type="email" name="email" placeholder="Enter your eMail"/>
+				<Field type="password" name="password" placeholder="Enter a password"/>
+				<Field type="checkbox" name="terms"/>
+				<button>Submit</button>
+			</Form>
 		</div>
 	)
 };
-export default Form;
+const UserFormikForm = withFormik(UserForm);
+export default UserFormikForm;
