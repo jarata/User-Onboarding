286694d0c3147493a07f725ef309f9e68d0b509c
Justin A <jarata@gmail.com>
2020-01-02 11:45:57 -0800

    adding more setup yarn add yup, axios

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+// Components
+import Form from "./components/Form";
+
 function App() {
   return (
     <div className="App">
       User Onboarding App
+      <Form/>
     </div>
   );
 }
