063cfa50e3f5bfa528d544fe21d5af3c1ddcb332
Justin A <jarata@gmail.com>
2020-01-03 16:18:23 -0800

    building layout on Form.js: added labels added ErrorMessage's withFormik: mapped props and default values setup validationSchema with Yup testing handleSubmit

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,19 +1,63 @@
 import React from 'react';
-import {withFormik, Field, Form} from "formik";
+import {withFormik, Field, Form, ErrorMessage} from "formik";
+import * as Yup from 'yup';
 
-const UserForm = () => {
+const UserForm = ({values}) => {
+	console.log("UserForm", values);
 	return (
 		<div>
 			Form
 			<Form>
-				<Field type="text" name="name" placeholder="Enter your name"/>
-				<Field type="email" name="email" placeholder="Enter your eMail"/>
-				<Field type="password" name="password" placeholder="Enter a password"/>
-				<Field type="checkbox" name="terms"/>
-				<button>Submit</button>
+				<label htmlFor="name">
+					Name:
+					<Field id="name" type="text" name="name" placeholder="Enter your name"/>
+					<ErrorMessage name="name"/>
+				</label>
+				<label>
+					eMail:
+					<Field id="email" type="email" name="email" placeholder="Enter your eMail"/>
+					<ErrorMessage name="email"/>
+				</label>
+				<label>
+					Password:
+					<Field id="password" type="password" name="password" placeholder="Enter a password"/>
+					<ErrorMessage name="password"/>
+				</label>
+				<label>
+					Terms of Service:
+					<Field id="terms" type="checkbox" name="terms" id="terms"/>
+					<ErrorMessage name="terms"/>
+				</label>
+				<button type="submit">Submit</button>
 			</Form>
 		</div>
 	)
 };
-const UserFormikForm = withFormik(UserForm);
+
+const UserFormikForm = withFormik({
+	mapPropsToValues({
+		                 name,
+		                 email,
+		                 password,
+		                 terms
+	}){
+		return {
+			name: name || "",
+			email: "",
+			password: "",
+			terms: false,
+		}
+	},
+	validationSchema: Yup.object().shape({
+		name: Yup.string().required().min(1),
+		email: Yup.string().required().email(),
+		password: Yup.string().required().min(6),
+		terms: Yup.boolean().oneOf([true], 'Must Accept Terms of Service')
+	}),
+	handleSubmit(values, formikBag) {
+		console.log("submitting!", values)
+	}
+})(
+	UserForm
+);
 export default UserFormikForm;
