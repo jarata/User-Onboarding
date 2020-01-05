9ac2978008bc378c60cc8face44b9e7a67f5e515
Justin A <jarata@gmail.com>
2020-01-05 11:58:41 -0800

    refactored UserForm: cleaned up code display added styling to Error's added labeling for accessibility added custom error messages added validation before submit

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,33 +1,30 @@
 import React from 'react';
-import {withFormik, Field, Form, ErrorMessage} from "formik";
+import {withFormik, Field, Form, ErrorMessage, validateYupSchema} from "formik";
+import axios from 'axios';
 import * as Yup from 'yup';
+import '../App.css';
 
 const UserForm = ({values}) => {
 	console.log("UserForm", values);
 	return (
 		<div>
-			Form
-			<Form>
-				<label htmlFor="name">
-					Name:
-					<Field id="name" type="text" name="name" placeholder="Enter your name"/>
-					<ErrorMessage name="name"/>
-				</label>
-				<label>
-					eMail:
-					<Field id="email" type="email" name="email" placeholder="Enter your eMail"/>
-					<ErrorMessage name="email"/>
-				</label>
-				<label>
-					Password:
-					<Field id="password" type="password" name="password" placeholder="Enter a password"/>
-					<ErrorMessage name="password"/>
-				</label>
-				<label>
-					Terms of Service:
-					<Field id="terms" type="checkbox" name="terms" id="terms"/>
-					<ErrorMessage name="terms"/>
-				</label>
+			<Form validationSchema={UserFormikForm.validationSchema}>
+				<label htmlFor="name">Name:</label>
+				<Field id="name" type="text" name="name" placeholder="Enter your name"/>
+				<ErrorMessage component="div" className="error" name="name"/>
+				<br/>
+				<label htmlFor="email">eMail:</label>
+				<Field id="email" type="email" name="email" placeholder="Enter your eMail"/>
+				<ErrorMessage component="div" className="error" name="email"/>
+				<br/>
+				<label htmlFor="password">Password:</label>
+				<Field id="password" type="password" name="password" placeholder="Enter a password"/>
+				<ErrorMessage component="div" className="error" name="password"/>
+				<br/>
+				<label htmlFor="terms">Terms of Service:</label>
+				<Field id="terms" type="checkbox" name="terms" id="terms"/>
+				<ErrorMessage component="div" className="error" name="terms"/>
+				<br/>
 				<button type="submit">Submit</button>
 			</Form>
 		</div>
@@ -49,9 +46,9 @@ const UserFormikForm = withFormik({
 		}
 	},
 	validationSchema: Yup.object().shape({
-		name: Yup.string().required().min(1),
-		email: Yup.string().required().email(),
-		password: Yup.string().required().min(6),
+		name: Yup.string().required("Please enter a name").matches(/^[a-zA-Z]+$/, { message: "Invalid name", excludeEmptyString: true }),
+		email: Yup.string().required("Please enter an email").email("Invalid email"),
+		password: Yup.string().required("Please enter a password").min(6, "Minimum 6 characters long"),
 		terms: Yup.boolean().oneOf([true], 'Must Accept Terms of Service')
 	}),
 	handleSubmit(values, formikBag) {
@@ -61,3 +58,4 @@ const UserFormikForm = withFormik({
 	UserForm
 );
 export default UserFormikForm;
+//
